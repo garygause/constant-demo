9310b90b0c14aab2669212736b18206ac16907c9
Gary Gause <gary@jennylife.com>
2022-01-08 13:21:52 -0800

    wrap sample data load in try block

--- services/api/manage.py
+++ services/api/manage.py
@@ -16,20 +16,26 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
+
+   # TODO: move lender sample data to external file
    db.session.add(Lender(1, "Apple Valley Inc", "100 Apple Lane", "Appleton", "WI", "54911", "9025551212"))
    db.session.add(Lender(2, "Industrial Heartland Credit Union", "1 Industrial Way", "Cleveland", "OH", "44101", "2165551212"))
    db.session.add(Lender(3, "Eastern Trust Company", "50 Wall Street", "New York", "NY", "10005", "9185551212"))
    db.session.add(Lender(4, "Golden National Bank", "561-A Main Street", "Golden", "CO", "80401", "3035551212"))
    db.session.add(Lender(5, "Island Lending", "100 Marginal Way", "Huntington", "NY", "11743", "6315551212"))
 
-   with open('./loans.csv', newline='') as f:
-       reader = csv.reader(f)
-       for row in reader:
-           db.session.add(Loan(
-              int(row[0]), int(row[1]), row[2], row[3], float(row[4]), int(row[5]), 
-              float(row[6]), int(row[7]), int(row[8]), float(row[9]), row[10]
-           ))
-
+   try:
+      with open('./loans.csv', newline='') as f:
+          reader = csv.reader(f)
+          for row in reader:
+              db.session.add(Loan(
+                 int(row[0]), int(row[1]), row[2], row[3], float(row[4]), int(row[5]), 
+                 float(row[6]), int(row[7]), int(row[8]), float(row[9]), row[10]
+              ))
+   except:
+      # TODO: add logging for errors
+      pass
+      
    db.session.commit()
 
 @cli.command("run_tests")
